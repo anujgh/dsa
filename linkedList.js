58b94a5b4b5e1f4eafc8ade159c83c87e217e1a8
anujgh <76443586+anujgh@users.noreply.github.com>
2024-09-11 13:59:32 +0530

    add unshift and shift functions

--- linkedList.js
+++ linkedList.js
@@ -43,13 +43,41 @@ class LinkedList {
         }
         return temp;
     }
+
+
+    unshift(value){
+        let newNode = new Node(value)
+        if(!this.head){
+            this.head = newNode
+            this.tail = newNode
+        } else {
+            newNode.next = this.head;
+            this.head = newNode
+        }
+        this.length++;
+        return this;
+    }
+
+    shift(){
+        if(!this.head) return undefined;
+        let temp = this.head;
+        this.head = this.head.next;
+        temp.next = null;
+        this.length--;
+        if(this.length === 0){
+            this.tail = null;
+        }
+        return temp;
+    }
     
     getList(){
         return this
     }
 }
 
-
+// --------------------
+// Testing.............
+// --------------------
 
 let myLinkedList = new LinkedList(5)
 myLinkedList.push(4)
@@ -60,3 +88,17 @@ console.log(myLinkedList.getList())
 const popedItem2 = myLinkedList.pop()
 console.log('popedItem2', popedItem2)
 console.log(myLinkedList)
+
+myLinkedList.unshift(3);
+myLinkedList.unshift(2);
+myLinkedList.unshift(1);
+console.log(myLinkedList.getList())
+
+console.log("shifted node: ",myLinkedList.shift())
+console.log("after shift the linked lis is: ",myLinkedList.getList())
+
+console.log("shifted node: ",myLinkedList.shift())
+console.log("after shift the linked lis is: ", myLinkedList.getList())
+
+console.log("shifted node: ",myLinkedList.shift())
+console.log("after shift the linked lis is: ", myLinkedList.getList())
